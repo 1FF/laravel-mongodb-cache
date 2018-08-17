97d5e6a1255737a22fdfa05c63ca38d8d3b80acc
Vihren Ganev <vihren@ganev.bg>
2018-08-17 17:34:13 +0300

    Fix expiration date

--- src/Store.php
+++ src/Store.php
@@ -55,7 +55,7 @@ class Store extends DatabaseStore
 
         $expirationSeconds = $cacheData['expiration']->toDateTime()->getTimestamp();
 
-        return ($expirationSeconds - time()) * 60;
+        return round(($expirationSeconds - time()) / 60);
     }
 
     /**
