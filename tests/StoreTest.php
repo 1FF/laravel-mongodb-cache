fb010ef5f0ba1a09c59e1ff0e47a660ace414cdb
Ivelin Ivanov <9060426+ivosgem@users.noreply.github.com>
2025-04-23 10:19:46 +0300

    Refactor StoreTest to use attributes for test annotations, update PHPUnit configuration and improve code type hints. Adjust .gitignore for PHPUnit cache directory and add ext-mongodb requirement in composer.json.

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -5,11 +5,12 @@ namespace Tests;
 use ForFit\Mongodb\Cache\MongoTaggedCache;
 use ForFit\Mongodb\Cache\Store;
 use Illuminate\Support\Carbon;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\Models\Cache;
 
 class StoreTest extends TestCase
 {
-    protected $store;
+    protected Store $store;
 
     protected function setUp(): void
     {
@@ -21,8 +22,8 @@ class StoreTest extends TestCase
         Carbon::setTestNow(now());
     }
 
-    /** @test */
-    public function it_stores_an_item_in_the_cache_for_given_time()
+    #[Test]
+    public function it_stores_an_item_in_the_cache_for_given_time(): void
     {
         // Act
         $sut = $this->store->put('test-key', 'test-value', 3);
@@ -37,8 +38,8 @@ class StoreTest extends TestCase
         ]);
     }
 
-    /** @test */
-    public function it_updates_an_item_in_the_cache_for_given_time()
+    #[Test]
+    public function it_updates_an_item_in_the_cache_for_given_time(): void
     {
         Cache::create([
             'key' => 'test-key',
@@ -58,8 +59,8 @@ class StoreTest extends TestCase
         ]);
     }
 
-    /** @test */
-    public function it_retrieves_value_from_the_cache_by_given_key()
+    #[Test]
+    public function it_retrieves_value_from_the_cache_by_given_key(): void
     {
         // Arrange
         Cache::create([
@@ -75,8 +76,8 @@ class StoreTest extends TestCase
         $this->assertEquals('test-value', $sut);
     }
 
-    /** @test */
-    public function it_returns_null_if_key_does_not_exist()
+    #[Test]
+    public function it_returns_null_if_key_does_not_exist(): void
     {
         // Act
         $sut = $this->store->get('test-key');
@@ -85,8 +86,8 @@ class StoreTest extends TestCase
         $this->assertNull($sut);
     }
 
-    /** @test */
-    public function it_sets_the_tags_to_be_used()
+    #[Test]
+    public function it_sets_the_tags_to_be_used(): void
     {
         // Act
         $sut = $this->store->tags(['tag1', 'tag2']);
@@ -96,8 +97,8 @@ class StoreTest extends TestCase
         $this->assertPropertySame(['tag1', 'tag2'], 'tags', $sut);
     }
 
-    /** @test */
-    public function it_deletes_all_records_with_the_given_tag()
+    #[Test]
+    public function it_deletes_all_records_with_the_given_tag(): void
     {
         // Arrange
         Cache::create([
@@ -126,8 +127,8 @@ class StoreTest extends TestCase
         ]);
     }
 
-    /** @test */
-    public function it_retrieves_an_items_expiration_time_by_given_key()
+    #[Test]
+    public function it_retrieves_an_items_expiration_time_by_given_key(): void
     {
         // Arrange
         Cache::create([
